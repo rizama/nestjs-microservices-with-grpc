b421c17966bf7737d6a4e750ea74ffcfc82897f3
Sam <rizky.pratama@sicepat.com>
2022-09-08 13:59:07 +0700

    add comment

--- grpc-nest-api-gateway/src/auth/auth.module.ts
+++ grpc-nest-api-gateway/src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { AuthService } from './auth.service';
         name: AUTH_SERVICE_NAME,
         transport: Transport.GRPC,
         options: {
-          url: '0.0.0.0:50051',
+          url: '0.0.0.0:50051', // url Auth Microservice
           package: AUTH_PACKAGE_NAME,
           protoPath: 'node_modules/grpc-nest-proto/proto/auth.proto',
         },
