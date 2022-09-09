67db63d88e95824dc7cffd8a197999572457135a
Sam <rizky.pratama@sicepat.com>
2022-09-09 10:20:41 +0700

    setup typeorm database

--- grpc-nest-product-svc/src/app.module.ts
+++ grpc-nest-product-svc/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ProductModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_product',
+      username: 'sam',
+      password: 'sam',
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
